e256490befdd7e40339cc47abd42422cde61c70b
Toni <funmitoni2@yahoo.co.uk>
2016-11-04 04:48:52 +0000

    sample code now compiles

--- src/vehicle_control.cpp
+++ src/vehicle_control.cpp
@@ -1,4 +1,4 @@
-nclude "ros/ros.h"
+#include "ros/ros.h"
 #include "std_msgs/String.h"
 
 #include <sstream>
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
    * The first NodeHandle constructed will fully initialize this node, and the last
    * NodeHandle destructed will close down the node.
    */
-  ros::NodeHandle n;
+  ros::NodeHandle n("~");
 
   /**
    * The advertise() function is how you tell ROS that you want to
