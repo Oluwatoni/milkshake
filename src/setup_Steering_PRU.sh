54fab95e3da728aa83549dd833292fb47f94635d
Toni <funmitoni2@yahoo.co.uk>
2016-11-12 23:07:45 +0000

    wrote up launch script and initialized the pwm

--- src/setup_Steering_PRU.sh
+++
@@ -1,3 +0,0 @@
-sudo cp PRU-STEERING-00A0.dtbo /lib/firmware
-sudo modprobe uio_pruss
-
--- src/vehicle_control.cpp
+++ src/vehicle_control.cpp
@@ -17,10 +17,12 @@
 #define PRU_NUM 	 0
 #define OFFSET_SHAREDRAM 2048		//equivalent with 0x00002000
 #define PRUSS0_SHARED_DATARAM    4
+#define STEERING_NEUTRAL 2048
 
 //function declaration
 static void Enable_PRU();
 static void Enable_ADC();
+static void Enable_PWM();
 static unsigned int ProcessingADC1(unsigned int value);
 
 //global variable declarations
@@ -31,9 +33,12 @@ const char SLOTS[] = "/sys/devices/bone_capemgr.9/slots";
 int main(int argc, char **argv)
 {
   unsigned int ret;
+  unsigned int steering_neutral = STEERING_NEUTRAL;
   tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+
   Enable_PRU();
   Enable_ADC();
+  Enable_PWM();
 
   ros::init(argc, argv, "vehicle_control");
   ros::NodeHandle n("~");
@@ -54,23 +59,22 @@ int main(int argc, char **argv)
   /*runs the code for a short period of time with the steering motor disabled
    to read from the ADC and clear the noise*/
   printf("Writing to ADC\n");
+  prussdrv_pru_write_memory(PRUSS0_PRU0_DATARAM, 0, &steering_neutral, 4);
   prussdrv_map_prumem(PRUSS0_PRU0_DATARAM, &sharedMem);
   sharedMem_int = (unsigned int* )sharedMem;
   *(sharedMem_int+1) = 2;
   prussdrv_exec_program (PRU_NUM, "./steering_pru.bin");
   usleep(100000);
   *(sharedMem_int+1) = 1;
-  printf("\tINFO: PRU completed transfer.\r\n");
+  printf("\tINFO: PRU completed initial test.\r\n");
   prussdrv_pru_clear_event (PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
 
   *(sharedMem_int+1) = 0;
+  prussdrv_pru_write_memory(PRUSS0_PRU0_DATARAM, 0, &steering_neutral, 4);
   prussdrv_exec_program (PRU_NUM, "./steering_pru.bin");
 
-  while (ros::ok())
-  {
-  
+  while (ros::ok()){
     ros::spinOnce();
-
     loop_rate.sleep();
   }
   //tells the PRU to halt
@@ -86,8 +90,7 @@ int main(int argc, char **argv)
 }
 
 /* Enable ADC */
-static void Enable_ADC()
-{
+static void Enable_ADC(){
   FILE *ain;
   ain = fopen(SLOTS, "w");
   if(!ain){
@@ -100,8 +103,7 @@ static void Enable_ADC()
 }
 
 /* Enable PRU */
-static void Enable_PRU()
-{
+static void Enable_PRU(){
   FILE *ain;
   ain = fopen(SLOTS, "w");
   if(!ain){
@@ -110,5 +112,19 @@ static void Enable_PRU()
   }
   fseek(ain, 0, SEEK_SET);
   fprintf(ain, "PRU-STEERING");
+  fflush(ain);
+}
+
+/* Enable PRU */
+static void Enable_PWM(){
+  FILE *ain;
+  ain = fopen(SLOTS, "w");
+  if(!ain){
+    printf("\tERROR: $SLOTS open failed\n");
+    return;
+  }
+  fseek(ain, 0, SEEK_SET);
+  fprintf(ain, "am33xx_pwm");
+  fprintf(ain, "bone_pwm_P9_28");
   fflush(ain);
 }
