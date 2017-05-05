3f92bf610009707e0a766f732aa1872336fbd517
Oluwatoni <funmitoni2@yahoo.co.uk>
2017-05-04 23:46:29 -0700

    Start on ADC for steering

--- Microcontroller Code/SteeringThrottleMCU/SteeringThrottleMCU/main.c
+++ Microcontroller Code/SteeringThrottleMCU/SteeringThrottleMCU/main.c
@@ -6,12 +6,13 @@
  */ 
 
 #include <avr/io.h>
-
+#include <stdint.h>
 
 int main(void)
 {
     //set up the timer interrupts for the steering and encoder
     //set up the SPI slave to receive data from the BBB
+    //set up the UART for debugging purposes and GPS
     while (1) 
     {
     }
--- Microcontroller Code/SteeringThrottleMCU/SteeringThrottleMCU/steering_throttle.c
+++ Microcontroller Code/SteeringThrottleMCU/SteeringThrottleMCU/steering_throttle.c
@@ -4,14 +4,22 @@
  * Created: 2017-05-04 9:58:13 PM
  *  Author: Oluwatoni
  */ 
+#define ADC_PIN 0
 #define ADC_RANGE 10
 #define ADC_SAMPLE_NO 8
-//Macro to read from the ADC
 
-//Macro to set the output logic
+void setupADC(){
+  //enables the ADC  ADEN = 1;  //right adjusted  ADLAR = 0;  //free running
+  
+}
 
 void steer(uint16_t steering_angle){
-  uint16_t current_angle = ;
+  uint16_t current_angle = 0;
+  
+  //end of a an ADC conversion
+  current_angle = ADCL;
+  //because the lower one must be read first
+  current_angle += (ADCH << 8);
   
   if (abs(steering_angle - current_angle) > ADC_RANGE){
     //correct
